e47185087fbc32bf440c6a28432f39573b61f63c
Geoff Ledak <geoffledak@gmail.com>
2023-06-25 17:57:48 -0400

    saving

--- Stepmania-Switcher/Stepmania-Switcher.cpp
+++ Stepmania-Switcher/Stepmania-Switcher.cpp
@@ -181,10 +181,47 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 
 
             case 1:
+            {
                 // Beep(40, 50);
-                ShellExecute(hWnd, L"open", L"notepad.exe", NULL, NULL, SW_SHOWNORMAL);
-                break;
+                // ShellExecute(hWnd, L"open", L"notepad.exe", NULL, NULL, SW_SHOWNORMAL);
 
+
+                STARTUPINFOW si = { 0 };
+                si.cb = sizeof(si);
+                PROCESS_INFORMATION pi = { 0 };
+
+                // Create the child process
+                BOOL success = CreateProcessW(
+                    L"C:\\Windows\\system32\\notepad.exe",  // Path to executable
+                    NULL,                                   // Command line arguments
+                    NULL,                                   // Process attributes
+                    NULL,                                   // Thread attributes
+                    FALSE,                                  // Inherit handles
+                    0,                                      // Creation flags
+                    NULL,                                   // Environment
+                    NULL,                                   // Working directory
+                    &si,                                    // Startup info
+                    &pi);                                   // Process information
+
+                if (success)
+                {
+                    // Wait for the process to exit
+                    WaitForSingleObject(pi.hProcess, INFINITE);
+
+                    // Process has exited - check its exit code
+                    DWORD exitCode;
+                    GetExitCodeProcess(pi.hProcess, &exitCode);
+
+                    // At this point exitCode is set to the process' exit code
+
+                    // Handles must be closed when they are no longer needed
+                    CloseHandle(pi.hThread);
+                    CloseHandle(pi.hProcess);
+                }
+
+
+                break;
+            }
             case IDM_ABOUT:
                 DialogBox(hInst, MAKEINTRESOURCE(IDD_ABOUTBOX), hWnd, About);
                 break;
